59e43e86d86232e721e89919d34fcaba5d69bb16
keedux <stevenpereira48@outlook.com>
2023-11-18 14:07:07 +0000

    game complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const [winner, setWinner] = useState(null);
+
   const [grid, setGrid] = useState([
     ["", "", ""],
     ["", "", ""],
@@ -11,29 +13,64 @@ function App() {
   const [player, setPlayer] = useState("X");
 
   function ClickSquare(rowIndex, itemIndex) {
-    console.log("row Index", rowIndex)
-    console.log("item Index", itemIndex)
     if (grid[rowIndex][itemIndex] !== "") {
       return;
+      
     }
-
+  
     const newGrid = [...grid];
     newGrid[rowIndex][itemIndex] = player;
     setGrid(newGrid);
     setPlayer(player === "X" ? "O" : "X");
+  
+    checkwinner();
+  }
+  
+  function checkwinner() {
+    const lines = [
+      [grid[0][0], grid[0][1], grid[0][2]], // top row
+      [grid[1][0], grid[1][1], grid[1][2]], // middle row
+      [grid[2][0], grid[2][1], grid[2][2]], // bottom row
+      [grid[0][0], grid[1][0], grid[2][0]], // left column
+      [grid[0][1], grid[1][1], grid[2][1]], // middle column
+      [grid[0][2], grid[1][2], grid[2][2]], // right column
+      [grid[0][0], grid[1][1], grid[2][2]], // main diagonal
+      [grid[0][2], grid[1][1], grid[2][0]]  // secondary diagonal
+    ];
+    console.log(lines);
 
-    console.log(grid);
+    for (let line of lines) {
+      if (line.every((value) => value === "X")) {
+        console.log("winner is X");
+        setWinner("X");
+        return;
+      } else if (line.every((value) => value === "O")) {
+        console.log("winner is O");
+        setWinner("O");
+        return;
+      }
+    }
+  
+    if (grid.every(row => row.every(value => value !== ""))) {
+      console.log("no winner");
+      setWinner(null);
+    }
   }
   return(
+  <>
+    <h1 className='title'>XO Game</h1>
+    <br/>
+    <h2 className='player'>Next Player: {winner ? `Winner is ${winner}` : "Draw"}</h2>
     <div className="App">
       {grid.map((row, rowIndex) => (
         <div key={rowIndex}>
           {row.map((item, itemIndex) => (
             <div className="square" onClick={() => ClickSquare(rowIndex, itemIndex)}>{item}</div>
           ))}
-        </div>
+      </div>
       ))}
     </div>
+  </>
   )
 }
 
